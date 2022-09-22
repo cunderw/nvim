540c1d1bf52d1ecbfba15792cc8c44eb59a77ea4
Carson Underwood <cunderw@gmail.com>
2022-09-22 17:31:38 -0500

    add eslint

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,155 +26,155 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
-    end,
-  },
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
 })
 
 return packer.startup(function(use)
-  use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
+	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
 
-  -- Appearance
-  use({ "akinsho/bufferline.nvim" })
-  use({ "kyazdani42/nvim-web-devicons" })
-  use({ "nvim-lualine/lualine.nvim" })
+	-- Appearance
+	use({ "akinsho/bufferline.nvim" })
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "nvim-lualine/lualine.nvim" })
 
-  -- Editor Utils
-  use({ "JoosepAlviste/nvim-ts-context-commentstring" })
-  use({ "itchyny/vim-highlighturl", event = "BufRead" })
-  use({ "karb94/neoscroll.nvim" })
-  use({ "lukas-reineke/indent-blankline.nvim" })
-  use({ "norcalli/nvim-colorizer.lua" })
-  use({ "numToStr/Comment.nvim" })
-  use({ "nvim-treesitter/nvim-treesitter" })
-  use({ "p00f/nvim-ts-rainbow" })
-  use({ "preservim/vimux" })
-  use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
-  use({ "phaazon/hop.nvim", branch = "v2" })
-  use({ "simrat39/symbols-outline.nvim" })
-  use({ "simrat39/inlay-hints.nvim" })
-  use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
-  use({ "RRethy/vim-illuminate" })
-  use({
-    "weilbith/nvim-code-action-menu",
-    cmd = "CodeActionMenu",
-  })
-  use({
-    "sudormrfbin/cheatsheet.nvim",
-    requires = {
-      { "nvim-telescope/telescope.nvim" },
-      { "nvim-lua/popup.nvim" },
-      { "nvim-lua/plenary.nvim" },
-    },
-  })
-  use({ "gennaro-tedesco/nvim-peekup" })
-  use({ 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' })
-  -- Projects / Sessions
-  use({ "folke/persistence.nvim", event = "BufReadPre", module = "persistence" })
-  use({ "ahmedkhalf/project.nvim" })
+	-- Editor Utils
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
+	use({ "itchyny/vim-highlighturl", event = "BufRead" })
+	use({ "karb94/neoscroll.nvim" })
+	use({ "lukas-reineke/indent-blankline.nvim" })
+	use({ "norcalli/nvim-colorizer.lua" })
+	use({ "numToStr/Comment.nvim" })
+	use({ "nvim-treesitter/nvim-treesitter" })
+	use({ "p00f/nvim-ts-rainbow" })
+	use({ "preservim/vimux" })
+	use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
+	use({ "phaazon/hop.nvim", branch = "v2" })
+	use({ "simrat39/symbols-outline.nvim" })
+	use({ "simrat39/inlay-hints.nvim" })
+	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+	use({ "RRethy/vim-illuminate" })
+	use({
+		"weilbith/nvim-code-action-menu",
+		cmd = "CodeActionMenu",
+	})
+	use({
+		"sudormrfbin/cheatsheet.nvim",
+		requires = {
+			{ "nvim-telescope/telescope.nvim" },
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+	})
+	use({ "gennaro-tedesco/nvim-peekup" })
+	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+	-- Projects / Sessions
+	use({ "folke/persistence.nvim", event = "BufReadPre", module = "persistence" })
+	use({ "ahmedkhalf/project.nvim" })
 
-  -- Misc utils
-  use({ "akinsho/toggleterm.nvim" })
-  use({ "christoomey/vim-tmux-navigator" })
-  use({ "folke/which-key.nvim" })
-  use({ "goolord/alpha-nvim" })
-  use({ "kyazdani42/nvim-tree.lua" })
-  use({ "lewis6991/impatient.nvim" })
-  use({ "moll/vim-bbye" })
-  use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
-  use({ "rcarriga/nvim-notify" })
-  use({ "vimpostor/vim-tpipeline" })
-  use({ "tiagovla/scope.nvim" })
+	-- Misc utils
+	use({ "akinsho/toggleterm.nvim" })
+	use({ "christoomey/vim-tmux-navigator" })
+	use({ "folke/which-key.nvim" })
+	use({ "goolord/alpha-nvim" })
+	use({ "kyazdani42/nvim-tree.lua" })
+	use({ "lewis6991/impatient.nvim" })
+	use({ "moll/vim-bbye" })
+	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
+	use({ "rcarriga/nvim-notify" })
+	use({ "vimpostor/vim-tpipeline" })
+	use({ "tiagovla/scope.nvim" })
 
-  -- Colorschemes
-  use({ "folke/tokyonight.nvim" })
-  use({ "tiagovla/tokyodark.nvim" })
+	-- Colorschemes
+	use({ "folke/tokyonight.nvim" })
+	use({ "tiagovla/tokyodark.nvim" })
 
-  -- Completion plugins
-  use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-  use({ "hrsh7th/cmp-cmdline" }) -- cmdline completions
-  use({ "hrsh7th/cmp-emoji" })
-  use({ "hrsh7th/cmp-nvim-lsp" })
-  use({ "hrsh7th/cmp-nvim-lua" })
-  use({ "hrsh7th/cmp-path" }) -- path completions
-  use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-  use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
+	-- Completion plugins
+	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
+	use({ "hrsh7th/cmp-cmdline" }) -- cmdline completions
+	use({ "hrsh7th/cmp-emoji" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-nvim-lua" })
+	use({ "hrsh7th/cmp-path" }) -- path completions
+	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
+	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
 
-  -- snippets
-  use({ "L3MON4D3/LuaSnip" }) --snippet engine
-  use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
+	-- snippets
+	use({ "L3MON4D3/LuaSnip" }) --snippet engine
+	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
-  -- LSP
-  use({ "neovim/nvim-lspconfig" }) -- enable LSP
-  use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
-  use({ "williamboman/nvim-lsp-installer" })
-  use({ "ray-x/lsp_signature.nvim" })
-  use({
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-  })
+	-- LSP
+	use({ "neovim/nvim-lspconfig" }) -- enable LSP
+	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
+	use({ "williamboman/nvim-lsp-installer" })
+	use({ "ray-x/lsp_signature.nvim" })
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+	})
 
-  -- Telescope
-  use({ "nvim-telescope/telescope.nvim" })
+	-- Telescope
+	use({ "nvim-telescope/telescope.nvim" })
 
-  -- Git
-  use({ "lewis6991/gitsigns.nvim" })
-  use({ "f-person/git-blame.nvim" })
-  use({ "ruifm/gitlinker.nvim" })
-  use({ "mattn/vim-gist" })
-  use({ "mattn/webapi-vim" })
+	-- Git
+	use({ "lewis6991/gitsigns.nvim" })
+	use({ "f-person/git-blame.nvim" })
+	use({ "ruifm/gitlinker.nvim" })
+	use({ "mattn/vim-gist" })
+	use({ "mattn/webapi-vim" })
 
-  -- Java
-  use({ "mfussenegger/nvim-jdtls" })
-  use({
-    'microsoft/java-debug',
-    opt = true,
-    run = 'mvnw clean install',
-  })
-  use({
-    'microsoft/vscode-java-test',
-    opt = true,
-    run = 'npm install && npm run build-plugin',
-  })
+	-- Java
+	use({ "mfussenegger/nvim-jdtls" })
+	use({
+		"microsoft/java-debug",
+		opt = true,
+		run = "mvnw clean install",
+	})
+	use({
+		"microsoft/vscode-java-test",
+		opt = true,
+		run = "npm install && npm run build-plugin",
+	})
 
-  -- Debug
-  use({ "mfussenegger/nvim-dap" })
-  use({ "rcarriga/nvim-dap-ui" })
-  use({
-    "nvim-neotest/neotest",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-neotest/neotest-go",
-    },
-  })
+	-- Debug
+	use({ "mfussenegger/nvim-dap" })
+	use({ "rcarriga/nvim-dap-ui" })
+	use({
+		"nvim-neotest/neotest",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-neotest/neotest-go",
+		},
+	})
 
-  -- Golang
-  use({ "benmills/vimux-golang" })
-  use({ "fatih/vim-go" })
-  use({ "rfratto/vim-go-testify" })
-  use({ "sebdah/vim-delve" })
-  use({ "crispgm/nvim-go" })
-  use({ "leoluz/nvim-dap-go" })
-  use { "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } }
+	-- Golang
+	use({ "benmills/vimux-golang" })
+	use({ "fatih/vim-go" })
+	use({ "rfratto/vim-go-testify" })
+	use({ "sebdah/vim-delve" })
+	use({ "crispgm/nvim-go" })
+	use({ "leoluz/nvim-dap-go" })
+	use({ "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } })
 
-  -- Typescript
-  use({ "jose-elias-alvarez/typescript.nvim" })
-  use({
-    "microsoft/vscode-js-debug",
-    opt = true,
-    run = "npm install --legacy-peer-deps && npm run compile"
-  })
-
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Typescript
+	use({ "jose-elias-alvarez/typescript.nvim" })
+	use({
+		"microsoft/vscode-js-debug",
+		opt = true,
+		run = "npm install --legacy-peer-deps && npm run compile",
+	})
+	use({ "MunifTanjim/eslint.nvim" })
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
--- lua/user/typescript.lua
+++ lua/user/typescript.lua
@@ -1,67 +1,96 @@
 local status_ok, typescript = pcall(require, "typescript")
 
 if not status_ok then
-  return
+	return
 end
 
 typescript.setup({})
 
-
 local status_ok, dapjs = pcall(require, "dap-vscode-js")
 
 if not status_ok then
-  return
+	return
 end
 
 dapjs.setup({
-  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+	-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+	-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+	adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
 })
 
 for _, language in ipairs({ "typescript", "javascript" }) do
-  require("dap").configurations[language] = {
-    {
-      type = "pwa-node",
-      request = "launch",
-      name = "Launch file",
-      program = "${file}",
-      cwd = "${workspaceFolder}",
-      sourceMaps = true,
-      protocol = "inspector",
-      console = "integratedTerminal",
-      -- outFiles = { "${workspaceFolder}/dist/**/*.js" },
-      resolveSourceMapLocations = { "${workspaceFolder}/dist/**/*.js", "${workspaceFolder}/**", "!**/node_modules/**" },
-      runtimeExecutable = "${workspaceFolder}/node_modules/.bin/ts-node",
-    },
-    {
-      type = "pwa-node",
-      request = "launch",
-      name = "PWA",
-      program = "${file}",
-      cwd = "${workspaceFolder}",
-    },
-    {
-      type = "pwa-node",
-      request = "attach",
-      name = "PWA Attach",
-      processId = require 'dap.utils'.pick_process,
-      cwd = "${workspaceFolder}",
-    },
-    {
-      type = "pwa-node",
-      request = "launch",
-      name = "Debug Jest Tests",
-      -- trace = true, -- include debugger info
-      runtimeExecutable = "node",
-      runtimeArgs = {
-        "./node_modules/jest/bin/jest.js",
-        "--runInBand",
-      },
-      rootPath = "${workspaceFolder}",
-      cwd = "${workspaceFolder}",
-      console = "integratedTerminal",
-      internalConsoleOptions = "neverOpen",
-    }
-  }
+	require("dap").configurations[language] = {
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch file",
+			program = "${file}",
+			cwd = "${workspaceFolder}",
+			sourceMaps = true,
+			protocol = "inspector",
+			console = "integratedTerminal",
+			-- outFiles = { "${workspaceFolder}/dist/**/*.js" },
+			resolveSourceMapLocations = {
+				"${workspaceFolder}/dist/**/*.js",
+				"${workspaceFolder}/**",
+				"!**/node_modules/**",
+			},
+			runtimeExecutable = "${workspaceFolder}/node_modules/.bin/ts-node",
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "PWA",
+			program = "${file}",
+			cwd = "${workspaceFolder}",
+		},
+		{
+			type = "pwa-node",
+			request = "attach",
+			name = "PWA Attach",
+			processId = require("dap.utils").pick_process,
+			cwd = "${workspaceFolder}",
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Debug Jest Tests",
+			-- trace = true, -- include debugger info
+			runtimeExecutable = "node",
+			runtimeArgs = {
+				"./node_modules/jest/bin/jest.js",
+				"--runInBand",
+			},
+			rootPath = "${workspaceFolder}",
+			cwd = "${workspaceFolder}",
+			console = "integratedTerminal",
+			internalConsoleOptions = "neverOpen",
+		},
+	}
 end
+
+local status_ok, eslint = pcall(require, "eslint")
+
+if not status_ok then
+	return
+end
+
+eslint.setup({
+	bin = "eslint", -- or `eslint_d`
+	code_actions = {
+		enable = true,
+		apply_on_save = {
+			enable = true,
+			types = { "problem" }, -- "directive", "problem", "suggestion", "layout"
+		},
+		disable_rule_comment = {
+			enable = true,
+			location = "separate_line", -- or `same_line`
+		},
+	},
+	diagnostics = {
+		enable = true,
+		report_unused_disable_directives = false,
+		run_on = "type", -- or `save`
+	},
+})
