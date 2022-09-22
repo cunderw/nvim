6c4eb217269224fc543becaf0483f7ba2b2abf1f
Carson Underwood <cunderw@gmail.com>
2022-09-22 16:21:27 -0500

    dapui tweaks

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,140 +1,150 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-  return
+	return
 end
 
 bufferline.setup({
-  options = {
-    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-    buffer_close_icon = "",
-    modified_icon = "●",
-    close_icon = "",
-    left_trunc_marker = "",
-    right_trunc_marker = "",
-    max_name_length = 30,
-    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-    tab_size = 21,
-    diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
-    offsets = {
-      { filetype = "NvimTree", text = "", padding = 1 }
-    },
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = "thick", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
-    always_show_bufferline = true,
-    custom_filter = function(buf_number, buf_numbers)
-      -- filter out filetypes you don't want to see
-      -- if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-      --   return true
-      -- end
-      -- filter out by buffer name
-      if vim.fn.bufname(buf_number) ~= "[dap-repl]" then
-        return true
-      end
-    end,
-    groups = {
-      items = {
-        {
-          name = "Proto",
-          highlight = { underline = true, sp = "blue" },
-          auto_close = true,
-          icon = "",
-          priority = 2,
-          matcher = function(buf)
-            return buf.name:match(".proto")
-          end,
-        },
-        {
-          name = "Tests",
-          highlight = { underline = true, sp = "blue" },
-          auto_close = true,
-          icon = "",
-          priority = 3,
-          matcher = function(buf)
-            return buf.name:match("%_test") or buf.name:match("%_spec")
-          end,
-        },
-      },
-    },
-  },
-  highlights = {
-    fill = {
-      fg = { attribute = "fg", highlight = "#ff0000" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    background = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    buffer_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
+	options = {
+		numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+		buffer_close_icon = "",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		max_name_length = 30,
+		max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+		tab_size = 21,
+		diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+		diagnostics_update_in_insert = false,
+		offsets = {
+			{ filetype = "NvimTree", text = "", padding = 1 },
+		},
+		show_buffer_icons = true,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		show_tab_indicators = true,
+		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+		separator_style = "thick", -- | "thick" | "thin" | { 'any', 'any' },
+		enforce_regular_tabs = true,
+		always_show_bufferline = true,
+		custom_filter = function(buf_number, buf_numbers)
+			-- filter out filetypes you don't want to see
+			-- if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+			--   return true
+			-- end
+			-- filter out by buffer name
+			-- if vim.fn.bufname(buf_number) ~= "[dap-repl]" then
+			-- 	return true
+			-- end
+		end,
+		groups = {
+			items = {
+				{
+					name = "Proto",
+					highlight = { underline = true, sp = "blue" },
+					auto_close = true,
+					icon = "",
+					priority = 2,
+					matcher = function(buf)
+						return buf.name:match(".proto")
+					end,
+				},
+				{
+					name = "Tests",
+					highlight = { underline = true, sp = "blue" },
+					auto_close = true,
+					icon = "",
+					priority = 3,
+					matcher = function(buf)
+						return buf.name:match("%_test") or buf.name:match("%_spec")
+					end,
+				},
+				{
+					name = "Repl",
+					highlight = { underline = true, sp = "blue" },
+					auto_close = true,
+					icon = "",
+					priority = 3,
+					matcher = function(buf)
+						return buf.name:match("[dap-repl]")
+					end,
+				},
+			},
+		},
+	},
+	highlights = {
+		fill = {
+			fg = { attribute = "fg", highlight = "#ff0000" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		background = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		buffer_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
-    close_button = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    close_button_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    tab_selected = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    tab = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    tab_close = {
-      fg = { attribute = "fg", highlight = "TabLineSel" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    duplicate_selected = {
-      fg = { attribute = "fg", highlight = "TabLineSel" },
-      bg = { attribute = "bg", highlight = "TabLineSel" },
-    },
-    duplicate_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    duplicate = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    modified = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    modified_selected = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    modified_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    separator = {
-      fg = { attribute = "bg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    separator_selected = {
-      fg = { attribute = "bg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    indicator_selected = {
-      fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-  },
+		close_button = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		close_button_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		tab_selected = {
+			fg = { attribute = "fg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		tab = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		tab_close = {
+			fg = { attribute = "fg", highlight = "TabLineSel" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		duplicate_selected = {
+			fg = { attribute = "fg", highlight = "TabLineSel" },
+			bg = { attribute = "bg", highlight = "TabLineSel" },
+		},
+		duplicate_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		duplicate = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		modified = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		modified_selected = {
+			fg = { attribute = "fg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		modified_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		separator = {
+			fg = { attribute = "bg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		separator_selected = {
+			fg = { attribute = "bg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		indicator_selected = {
+			fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+	},
 })
