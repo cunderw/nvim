73f8d47b280636840529fead1cc9287e70532071
Carson Underwood <carson.underwood.ext@bayer.com>
2022-09-29 16:57:08 -0500

    fix go loading launch.json

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -8,8 +8,13 @@ local icons = require("user.icons")
 
 vim.fn.sign_define("DapBreakpoint", { text = icons.ui.Bug, texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
+local status_ok, dapui = pcall(require, "dapui")
+if not status_ok then
+  return
+end
+
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  require("dapui").open({})
+  dapui.open({})
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
   -- dapui.close {}
@@ -17,9 +22,3 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   -- dapui.close {}
 end
-
-dap.adapters.go = {
-  type = 'executable';
-  command = 'node';
-  args = { os.getenv('HOME') .. '/.local/share/nvim/site/pack/packer/opt/vscode-go/dist/debugAdapter.js' };
-}
